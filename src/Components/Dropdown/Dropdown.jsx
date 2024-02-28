309ec0461a718bbb8fd6cdd20fde3cb04ef55b84
93mcqmil <mcqueen_1993@hotmail.com>
2024-02-28 13:13:27 +0100

    small fix

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -1,11 +1,13 @@
 import "../Dropdown/Dropdown.css";
+//funktion för min dropdown menu
 
 function Dropdownmenu() {
   return (
     <>
-      <label htmlFor='name-select'>Choose a character: </label>;
-      <select name='characters' id='charachter-select'></select>
-      <option value=''>--Please choose an option</option>
+      <label htmlFor='name-select'>Choose a character: </label>
+      <select name='characters' id='charachter-select'>
+        <option value=''>--Please choose an option</option>
+      </select>
     </>
   );
 }
